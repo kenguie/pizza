b35cb390bf1e33deeae1bc85dd87c7114bc50d1f
Jason <yimjason@aol.com>
2014-09-29 16:00:12 -0400

    reviews

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
+require 'mandrill' 
 
-get '/' do 
+get '/' do  
 	erb :index
 end 
 
+post '/contact' do
+ 	m = Mandrill::API.new
+	message = { 
+	 :subject=> "comment", 
+	 :from_name=> params[:name], 
+	 :text=>params[:comment],
+	 :to=>[{:email=> "nyhunter77@yahoo.com", :name=> "Ken"}], 
+	 :html=>"<html><h1>Hi <strong>message</strong>, #{:comment}</h1></html>", 
+	 :from_email=>params[:email] 
+	} 
+	sending = m.messages.send message 
+	puts 'my params are' + params.inspect
+end
 
  
