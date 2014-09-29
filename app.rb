bf5d8ec6ab3f9bf46d3f6597184557f63d03511a
Jason <yimjason@aol.com>
2014-09-29 16:32:59 -0400

    test

--- app.rb
+++ app.rb
@@ -5,19 +5,19 @@ get '/' do
 	erb :index 
 end 
 
-post '/contact' do
- 	m = Mandrill::API.new
-	message = { 
-	 :subject=> "comment", 
-	 :from_name=> params[:name], 
-	 :text=>params[:comment],
-	 :to=>[{:email=> "nyhunter77@yahoo.com", :name=> "Ken"}], 
-	 :html=>"<html><h1>Hi <strong>message</strong>, #{params[:comment]}</h1></html>", 
-	 :from_email=>params[:email] 
-	} 
-	sending = m.messages.send message 
-	puts 'my params are' + params.inspect 
-end
+# post '/contact' do
+#  	m = Mandrill::API.new
+# 	message = { 
+# 	 :subject=> "comment", 
+# 	 :from_name=> params[:name], 
+# 	 :text=>params[:comment],
+# 	 :to=>[{:email=> "nyhunter77@yahoo.com", :name=> "Ken"}], 
+# 	 :html=>"<html><h1>Hi <strong>message</strong>, #{params[:comment]}</h1></html>", 
+# 	 :from_email=>params[:email] 
+# 	} 
+# 	sending = m.messages.send message 
+# 	puts 'my params are' + params.inspect 
+# end
 
 get '/story' do
 	erb :ourStory
